b9d70a41c9c266b7d5a218af1135d37f0546996d
samar-01 <66644907+samar-01@users.noreply.github.com>
2023-06-19 16:23:51 -0400

    submodule for nexstar

--- build.rs
+++ build.rs
@@ -5,12 +5,8 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-	// println!("cargo:rustc-link-lib=opencv_core");
-	// println!("cargo:rerun-if-changed=build.rs");
-	// println!("cargo:rerun-if-changed=path/to/Cargo.lock");
-
 	println!("cargo:rerun-if-changed=src/teleC.cpp");
-	cc::Build::new().file("src/teleC.cpp").file("src/nexstar.c").include("src/libnexstar/src").compile("teleC");
+	cc::Build::new().file("src/teleC.cpp").file("src/libnexstar/nexstar.c").compile("teleC");
 	let bindings = bindgen::Builder::default()
 		.header("src/teleC.cpp")
 		.parse_callbacks(Box::new(bindgen::CargoCallbacks))
